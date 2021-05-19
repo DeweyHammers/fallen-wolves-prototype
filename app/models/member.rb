51628c733c10f78279988efeacf9152dfd2e4567
Dewey <hammersdrichard@gmail.com>
2021-05-19 09:33:39 -0700

    add member model to the app

---
+++ app/models/member.rb
@@ -0,0 +1,10 @@
+class Member < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :comment_posts, through: :posts, source: :post
+  has_one_atttached :avatar
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest,presence: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true
+end
---
+++ db/migrate/20210519163011_create_members.rb
@@ -0,0 +1,12 @@
+class CreateMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :members do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :rank
+
+      t.timestamps
+    end
+  end
+end
