6bc73223ee4d900b468c8b14ced277da51eb619a
dewey <hammersdrichard@gmail.com>
2021-09-07 12:25:38 -0700

    g

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "bootstrap";
 import "../stylesheets/application";
-import "../../assets/stylesheets";
 
 Rails.start();
 Turbolinks.start();
